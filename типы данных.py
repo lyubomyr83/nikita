3380b36985ba9f77ff39bd245a70fc737cad99b2
lyubomyr <lyubomyr83@mail.ru>
2021-01-29 16:02:45 +0300

    типы данных

---
+++ типы данных.py
@@ -0,0 +1,8 @@
+a = 5  # integer - целое число int()
+n = 'Никита'  # string - стока/текст str()
+pi = 3.14  # float - число с плавающей точкой float()
+b = True  # boolean - логический тип True/False bool()
+c = None  # None
+
+digit = input('Введи цифру:\n')  # приходит в типе string '0'
+print(bool(int(digit)))  
